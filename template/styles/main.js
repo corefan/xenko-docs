c1bfa3c6ac0ac034bd364459f59fc56ba0572880
Andrew Shengilevych <ashengilevych@n-ix.com>
2017-02-08 19:17:20 +0200

    change class page layout, add script to relink items

--- template/styles/main.js
+++ template/styles/main.js
@@ -7,4 +7,18 @@ $(function() {
   
   // Captions are not automatically shown anymore by default; uncomment to reenable them
   //showCaptionFromAlt("article img");
+
+  // Get links from table and set it to code wrapper
+  $('.xk-hidden-table').each(function(){
+    $(this).find('a.xref').each(function(){
+      var variableLinkText = $(this).text();
+      var variableLink = $(this).attr('href');
+      var variableResult = '<a class="xref" href="' + variableLink + '">' + variableLinkText + '</a>';
+      $('.codewrapper').each(function(){
+        if($(this).html().indexOf(variableLinkText) > -1)
+        var variableCode = $(this).html().replace(variableLinkText, variableResult);
+        $(this).html(variableCode);
+      });
+    });
+  });
 });
